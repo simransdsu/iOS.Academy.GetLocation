322a5ce33305ac548d22c76316f2e06eba97e82a
Simran Preet Singh <simran5590@gmail.com>
2020-11-17 19:56:47 -0800

    Getting location

--- GetLocation/ViewController.swift
+++ GetLocation/ViewController.swift
@@ -1,10 +1,37 @@
 import UIKit
+// Step 1
+import CoreLocation
 
-class ViewController: UIViewController {
+// Step 2
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var label: UILabel!
     
+    // Step 3
+    var manager: CLLocationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Step 0
+        label.text = "ready!"
+        
+        // Step 4
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
     }
+    
+    // Step 5
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]) {
+        guard let first = locations.first else {
+            return
+        }
+        
+        label.text = "\(first.coordinate.longitude) : \(first.coordinate.latitude)"
+    }
+    
+    // Step 6 is in Info.plit
 }
